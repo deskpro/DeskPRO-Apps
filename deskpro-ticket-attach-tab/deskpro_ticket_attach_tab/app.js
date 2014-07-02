44513a9f5e0708e5814a8f42804dbaad169da52a
Christopher Nadeau <chris@nadeau.ws>
2014-07-02 16:19:52 +0100

    Add drag+drop downloading of attachments

--- deskpro-ticket-attach-tab/deskpro_ticket_attach_tab/app.js
+++ deskpro-ticket-attach-tab/deskpro_ticket_attach_tab/app.js
@@ -3,7 +3,6 @@ define(function() {
 		init: function() {
 			this.registerWidgetTab('ticket', '@messages.tab', "Attachments ({{content.attachments.length}})", 'tab.html', function($scope, $app, $ticket) {
 				$scope.attachments = $ticket.attachments;
-				console.log($scope.attachments);
 			});
 		}
 	}
